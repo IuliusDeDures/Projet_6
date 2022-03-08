873ae34b57f88878b8f333c8fdb5bbac523150d4
IuliusDeDures <julien.bauland49@orange.fr>
2022-03-08 15:23:19 +0100

    ajout d'express-rate-limit pour limiter le nombres de requêtes, ajout de mongo-sanitize pour lutter contre les injections et modification de helmet pour améliorer la protection du site

--- backend/app.js
+++ backend/app.js
@@ -1,57 +1,67 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const helmet = require('helmet');
+const helmet = require("helmet");
 const bodyParser = require("body-parser");
-const mongoose = require('mongoose');
-const sauceRoutes = require('./routes/sauce');
-const userRoutes = require('./routes/user');
-const path = require('path');
-var sixtyDaysInSeconds = 5184000;
+const mongoose = require("mongoose");
+const sauceRoutes = require("./routes/sauce");
+const userRoutes = require("./routes/user");
+const path = require("path");
+const mongoSanitize = require("express-mongo-sanitize");
+const rateLimit = require("express-rate-limit");
 
-require('dotenv').config()
+// configuration du limiteur de requête
+const limiteur = rateLimit({
+    windowMs: 15 * 60 * 1000, // période 15 minutes
+    max: 100, // limite à 100 requête pour 15 minutes
+    standardHeaders: true,
+    legacyHeaders: false,
+});
+
+require("dotenv").config();
 
 // connection à la base de donnée Mongo DB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+    .connect(process.env.MONGO_URI, {
         useNewUrlParser: true,
-        useUnifiedTopology: true
+        useUnifiedTopology: true,
     })
-    .then(() => console.log('Connexion à MongoDB réussie !'))
-    .catch(() => console.log('Connexion à MongoDB échouée !'));
-
+    .then(() => console.log("Connexion à MongoDB réussie !"))
+    .catch(() => console.log("Connexion à MongoDB échouée !"));
 
 app.use(bodyParser.json());
+
+// Utilisation de helmet pour sécuriser le site contre certaines vulnirabilités connues
+app.use(helmet());
+
+// Utilisation de mongo sanitize pour lutter contre les injections
+app.use(mongoSanitize({ replaceWith: "_" }));
+
+// application du middleware de limitation de debit à toutes les requêtes 
+app.use(limiteur);
 
 // middleware pour confiqurer les headers des requêtes
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Origin", "*");
     res.setHeader(
         "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization");
+        "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
     res.setHeader(
         "Access-Control-Allow-Methods",
         "GET, POST, PUT, DELETE, PATCH, OPTIONS"
     );
+    res.setHeader("Cross-Origin-Resource-Policy", "cross-origin");
 
     next();
 });
 
-// Utilisation de helmet pour sécurisation du site :
-
-app.use(helmet.xssFilter()); // contre attaques cross-site scripting et injections intersites
-app.use(helmet.frameguard({ action: 'deny' })); // contre attaques clickjacking
-app.use(helmet.hsts({ maxAge: sixtyDaysInSeconds })); // pour chiffrer les transmission contre les attaques homme du milieu (MITM)
-app.use(helmet.contentSecurityPolicy()); // contre attaques cross-site scripting et injections intersites
-app.use(helmet.hidePoweredBy()); // contre les attaques spécifiquement ciblées sur Express
-app.use(helmet.noSniff()); // protège les navigateur du reniflage du code MINE (contre attaques cross-site scripting)
-
-
 // middleware pour la gestion d'image de manière statique
-app.use('/images', express.static(path.join(__dirname, 'images')));
+app.use("/images", express.static(path.join(__dirname, "images")));
 
 // middleware pour les routes sauces
-app.use('/api/sauces', sauceRoutes);
+app.use("/api/sauces", sauceRoutes);
 
 // middleware pour les routes users
-app.use('/api/auth', userRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;
